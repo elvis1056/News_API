9ac12c69fe53a5f093347aed4c4e34f25d7914cd
elvis1056 <john800116@hotmail.com>
2019-04-17 11:06:12 +0800

    Updates

---
+++ precache-manifest.8e4883afbd719e6a03dccbb4712c43c7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "9696192cf6de91e06a72",
+    "url": "/News_API/static/js/runtime~main.a2c734c3.js"
+  },
+  {
+    "revision": "210a7cca332bfa96e162",
+    "url": "/News_API/static/js/main.14891272.chunk.js"
+  },
+  {
+    "revision": "e1321264158d8c573e20",
+    "url": "/News_API/static/js/2.b67d4811.chunk.js"
+  },
+  {
+    "revision": "e90257d7843eb1bf0fa5d35812d87459",
+    "url": "/News_API/index.html"
+  }
+];
